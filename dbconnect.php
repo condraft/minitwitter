1fbb1404075047779894ec769d6a504be33e844b
condraft <55941179+condraft@users.noreply.github.com>
2020-01-09 19:07:42 +0900

    cleadb

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,14 @@
 <?php
 try{
-    $db = new PDO('mysql:dbname=mini_bbs;host=127.0.0.1;
-    charset=utf8', 'root', '');
+    $db = new PDO('mysql:dbname=heroku_7e20f61e42162e2;host=us-cdbr-iron-east-05.cleardb.net;
+    charset=utf8', 'b8c6954338d34a', '6ebef33f');
 }catch(PDOException $e){
     print('DBconnectError:' . $e->getMessage());
 }
+// <?php
+// try{
+//     $db = new PDO('mysql:dbname=mini_bbs;host=127.0.0.1;
+//     charset=utf8', 'root', '');
+// }catch(PDOException $e){
+//     print('DBconnectError:' . $e->getMessage());
+// }
